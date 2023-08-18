9a7b72caa418f443357d783c571e2c6b5c7885b8
Suibbsu <142184204+Suibbsu@users.noreply.github.com>
2023-08-18 11:00:24 +0900

    Initial commit

---
+++ src/main/java/kr/or/comeeat/ComeEatApplication.java
@@ -0,0 +1,13 @@
+package kr.or.comeeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeEatApplication.class, args);
+	}
+
+}
