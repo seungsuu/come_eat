85bb12fb2145e170a6d67294e825b592a46a5fae
Suibbsu <142184204+Suibbsu@users.noreply.github.com>
2023-08-18 16:51:41 +0900

    sweetalert 추가

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,14 @@
+
+function alertFunc(icon){
+	swal({
+        title: '제목',
+        text: '내용내용내용',
+        icon: icon
+      });
+      //이후 페이지 이동 시 추가
+      /*
+      .then(function(){
+    	 window.location = "${loc}"; 
+      });
+      */
+}
