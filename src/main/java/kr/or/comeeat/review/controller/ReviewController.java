e1b0ab53c8b0d42fb2069fd0be4a0f78b698328d
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-08-23 16:47:32 +0900

    search 구현2

--- src/main/java/kr/or/comeeat/review/controller/ReviewController.java
+++ src/main/java/kr/or/comeeat/review/controller/ReviewController.java
@@ -1,11 +1,15 @@
 package kr.or.comeeat.review.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.comeeat.location.model.vo.Location;
 import kr.or.comeeat.review.model.service.ReviewService;
 
 @Controller
@@ -17,7 +21,9 @@ public class ReviewController {
 
 	
 	@GetMapping(value="/detailRestaurant")
-	public String detailRestaurant() {
+	public String detailRestaurant(String loTitle,Model model) {
+		Location l = reviewService.selectOneRestaurant(loTitle);
+		model.addAttribute("list",l);
 		return "review/detailRestaurant";
 	}
 	
--- src/main/java/kr/or/comeeat/review/model/service/ReviewService.java
+++ src/main/java/kr/or/comeeat/review/model/service/ReviewService.java
@@ -1,11 +1,21 @@
 package kr.or.comeeat.review.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.comeeat.location.model.vo.Location;
 import kr.or.comeeat.review.model.dao.ReviewDao;
 
 @Service
 public class ReviewService {
 
+	@Autowired
 	private ReviewDao reviewDao;
+
+	public Location selectOneRestaurant(String loTitle) {
+		Location l = reviewDao.selectOneRestaurant(loTitle);
+		return l;
+	}
 }
