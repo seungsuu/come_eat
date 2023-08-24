9e0eb0781e3dfa11c7e00f98c0df56fc06f4c4ea
Suibbsu <142184204+Suibbsu@users.noreply.github.com>
2023-08-24 16:51:43 +0900

    booking끝

--- src/main/java/kr/or/comeeat/booking/model/dao/BookingDao.java
+++ src/main/java/kr/or/comeeat/booking/model/dao/BookingDao.java
@@ -4,8 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.or.comeeat.booking.model.vo.Booking;
+
 @Repository
 public class BookingDao {
 	@Autowired
 	private JdbcTemplate jdbc;
+
+	public int insertBooking(Booking b) {
+		String query = "insert into booking values(booking_seq.nextval,?,?,?,?,?)";
+		Object[] params = {b.getBookingTime(),b.getBookingDate(),b.getBookingTotalnum(),b.getMemberNo(), b.getMemberName()};
+		int result = jdbc.update(query, params);
+		return result;
+	}
 }
