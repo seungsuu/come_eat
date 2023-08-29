f635474cc50cf66b9c30506ea3df851e8b45bc6b
co3148 <142196451+co3148@users.noreply.github.com>
2023-08-29 18:03:57 +0900

    event게시판 글쓰기 더보기

--- src/main/java/kr/or/comeeat/WebConfig.java
+++ src/main/java/kr/or/comeeat/WebConfig.java
@@ -17,5 +17,9 @@ public class WebConfig implements WebMvcConfigurer{
 		
 		registry.addResourceHandler("/editor/**").addResourceLocations("file:///C:/Temp/upload/editor/");
 		registry.addResourceHandler("/location/**").addResourceLocations("classpath:/static/img/location/");
+		
+		registry.addResourceHandler("/event/**")
+		.addResourceLocations("file:///C:Temp/upload/event/");
+		
 	}
 }
